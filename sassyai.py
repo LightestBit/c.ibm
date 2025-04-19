2d8b4267421a0c56fb32587e6394a42f9d4f7d40
LightestBit <chukwuemeka.agwaziam@gmail.com>
2025-04-18 19:27:42 -0600

    Add files via upload

---
+++ sassyai.py
@@ -0,0 +1,18 @@
+print("Hello, person.")
+borsht = input("Enter your age in years. Whole numbers. Now. Chop chop, I haven't all day...  ")
+print("I'm calculating, leave me alone for 3 seconds...")
+aisy = float(borsht) * 31536000
+print("Okay, kid, you've been alive for at least " + str(aisy) + " seconds. Happy?")
+print("What else do you want?")
+print()
+print("Days too? Man, this generation... Fine.")
+aitd = float(borsht) * 365
+print("You've been alive for at least " + str(aitd) + " days")
+print("  ")
+
+pounds = input("What's your weight (in pounds, please)? ")
+print("Converting " + pounds + " pounds to kilograms now... ")
+print("Please wait...")
+kilo_grams = float(pounds) * 0.45
+print ("You are about " + str(kilo_grams) + ' kilograms')
+print()
