e85b3e64e6fd13acbd048b5dbadae72d89c0a048
LightestBit <chukwuemeka.agwaziam@gmail.com>
2025-04-18 19:29:55 -0600

    Update sassyai.py

--- sassyai.py
+++ sassyai.py
@@ -1,6 +1,7 @@
 print("Hello, person.")
 borsht = input("Enter your age in years. Whole numbers. Now. Chop chop, I haven't all day...  ")
 print("I'm calculating, leave me alone for 3 seconds...")
+# 31536000 is seconds/year
 aisy = float(borsht) * 31536000
 print("Okay, kid, you've been alive for at least " + str(aisy) + " seconds. Happy?")
 print("What else do you want?")
@@ -15,4 +16,4 @@ print("Converting " + pounds + " pounds to kilograms now... ")
 print("Please wait...")
 kilo_grams = float(pounds) * 0.45
 print ("You are about " + str(kilo_grams) + ' kilograms')
-print()
+print("Done? Good. Now let me sleep.")
